5a395bd04f1193b2962c7cc94b713ffccd503a23
muneebhashone <muneeb.hashone@gmail.com>
2024-08-01 06:50:55 +0500

    added list car booking query

--- src/lib/common.schema.ts
+++ src/lib/common.schema.ts
@@ -3,8 +3,8 @@ import z from 'zod';
 
 export const searchAndPaginationSchema = z.object({
   search: z.string().optional(),
-  page: z.string().transform(Number).optional(),
-  limit: z.string().transform(Number).optional(),
+  page: z.string().default('1').transform(Number).optional(),
+  limit: z.string().default('10').transform(Number).optional(),
 });
 
 export const returnMessageSchema = z.object({
