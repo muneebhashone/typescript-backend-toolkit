1e1c9123c4169f4f0a5e686a8feae3de0bfcbdd9
muneebhashone <muneeb.hashone@gmail.com>
2024-08-01 05:31:10 +0500

    fix zod validations for id

--- src/car/car-listing/car-listing.schema.ts
+++ src/car/car-listing/car-listing.schema.ts
@@ -14,7 +14,7 @@ export const carIdSchema = z.object({
   id: z
     .string({ required_error: 'ID is required' })
     .min(1)
-    .refine((value) => validator.isAlphanumeric(value), 'ID must be valid'),
+    .refine((value) => validator.isMongoId(value), 'ID must be valid'),
 });
 
 export const carListQueryParamsSchema = z
