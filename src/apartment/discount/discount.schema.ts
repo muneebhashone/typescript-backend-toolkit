d95d54765c742e2a72bf9d55d64d371253fa996f
muneebhashone <muneeb.hashone@gmail.com>
2024-08-01 05:31:10 +0500

    fix zod validations for id

--- src/apartment/discount/discount.schema.ts
+++ src/apartment/discount/discount.schema.ts
@@ -5,7 +5,7 @@ export const discountIdSchema = z.object({
   id: z
     .string({ required_error: 'ID is required' })
     .min(1)
-    .refine((value) => validator.isAlphanumeric(value), 'ID must be valid'),
+    .refine((value) => validator.isMongoId(value), 'ID must be valid'),
 });
 
 export const discountCreateOrUpdateSchema = z.object({
