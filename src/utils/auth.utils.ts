d0138052d413083cd2df91fc15dc117d05a776b6
Abdul-Hadi-Waseem <shaikhahw54@gmail.com>
2024-08-03 00:55:31 +0500

    feat: implemented google login/signup functionality

--- src/utils/auth.utils.ts
+++ src/utils/auth.utils.ts
@@ -1,9 +1,22 @@
 import argon2 from 'argon2';
 import { JsonWebTokenError, sign, verify } from 'jsonwebtoken';
 import config from '../config/config.service';
-import { RoleType } from '../drizzle/enums';
 import logger from '../lib/logger.service';
 import crypto from 'node:crypto';
+import { RoleType } from '../enums';
+
+export interface GoogleTokenResponse {
+  access_token: string;
+  expires_in: number;
+  id_token: string;
+  refresh_token?: string;
+  scope: string;
+  token_type: string;
+}
+
+export interface GoogleTokensRequestParams {
+  code: string;
+}
 
 export type JwtPayload = {
   sub: string;
@@ -78,4 +91,50 @@ export const verifyToken = async <
 
 export const generateRandomPassword = (length: number = 16): string => {
   return crypto.randomBytes(length).toString('hex');
+};
+export const fetchGoogleTokens = async (
+  params: GoogleTokensRequestParams,
+): Promise<GoogleTokenResponse> => {
+  const url = 'https://oauth2.googleapis.com/token';
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    body: new URLSearchParams({
+      code: params.code,
+      client_id: process.env.GOOGLE_CLIENT_ID,
+      client_secret: process.env.GOOGLE_CLIENT_SECRET,
+      redirect_uri: process.env.REDIRECT_URI,
+      grant_type: 'authorization_code',
+    }),
+  });
+
+  if (!response.ok) {
+    throw new Error('Failed to exchange code for tokens');
+  }
+
+  const data: GoogleTokenResponse = await response.json();
+  return data;
+};
+export interface GoogleUserInfo {
+  id: string; // User's unique Google ID
+  email: string; // User's email address
+  verified_email: boolean; // Whether the email is verified
+  name: string; // User's full name
+  given_name: string; // User's given name
+  family_name: string; // User's family name
+  picture: string; // URL of the user's profile picture
+  locale: string; // User's locale
+}
+
+export const getUserInfo = async (accessToken: string) => {
+  const userInfoResponse = await fetch(
+    'https://www.googleapis.com/oauth2/v2/userinfo',
+    {
+      headers: { Authorization: `Bearer ${accessToken}` },
+    },
+  );
+  if (!userInfoResponse.ok) {
+    throw new Error(`Error fetching user info`);
+  }
+  return userInfoResponse.json();
 };
