d3f5b93244c376974f2ef69398855b3c83086757
muneebhashone <themuneebh@gmail.com>
2025-10-12 01:37:40 +0500

    refactor: standardize quote style in can-access middleware for consistency

--- src/middlewares/can-access.ts
+++ src/middlewares/can-access.ts
@@ -1,91 +1,91 @@
-import type { NextFunction } from "express";
-import { StatusCodes } from "http-status-codes";
-import type { RoleType } from "../enums";
-import { getUserById } from "../modules/user/user.services";
-import { errorResponse } from "../utils/api.utils";
-import type { JwtPayload } from "../utils/auth.utils";
-import type { RequestAny, ResponseAny } from "../openapi/magic-router";
+import type { NextFunction } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import type { RoleType } from '../enums';
+import { getUserById } from '../modules/user/user.services';
+import { errorResponse } from '../utils/api.utils';
+import type { JwtPayload } from '../utils/auth.utils';
+import type { RequestAny, ResponseAny } from '../openapi/magic-router';
 
-export type CanAccessByType = "roles";
+export type CanAccessByType = 'roles';
 
 export type CanAccessOptions = {
-	roles: RoleType | "*";
+  roles: RoleType | '*';
 };
 
 export const canAccess =
-	<T extends CanAccessByType>(by?: T, access?: CanAccessOptions[T][]) =>
-	async (req: RequestAny, res: ResponseAny, next?: NextFunction) => {
-		try {
-			const requestUser = req?.user as JwtPayload;
+  <T extends CanAccessByType>(by?: T, access?: CanAccessOptions[T][]) =>
+  async (req: RequestAny, res: ResponseAny, next?: NextFunction) => {
+    try {
+      const requestUser = req?.user as JwtPayload;
 
-			if (!requestUser) {
-				return errorResponse(
-					res,
-					"token isn't attached or expired",
-					StatusCodes.UNAUTHORIZED,
-				);
-			}
-			const currentUser = await getUserById(requestUser.sub);
+      if (!requestUser) {
+        return errorResponse(
+          res,
+          "token isn't attached or expired",
+          StatusCodes.UNAUTHORIZED,
+        );
+      }
+      const currentUser = await getUserById(requestUser.sub);
 
-			if (!currentUser) {
-				return errorResponse(res, "Login again", StatusCodes.UNAUTHORIZED);
-			}
+      if (!currentUser) {
+        return errorResponse(res, 'Login again', StatusCodes.UNAUTHORIZED);
+      }
 
-			if (currentUser.otp !== null) {
-				return errorResponse(
-					res,
-					"Your account is not verified",
-					StatusCodes.UNAUTHORIZED,
-				);
-			}
+      if (currentUser.otp !== null) {
+        return errorResponse(
+          res,
+          'Your account is not verified',
+          StatusCodes.UNAUTHORIZED,
+        );
+      }
 
-			let can = false;
+      let can = false;
 
-			const accessorsToScanFor = access;
+      const accessorsToScanFor = access;
 
-			if (by === "roles" && accessorsToScanFor) {
-				can = (accessorsToScanFor as RoleType[]).includes(
-					currentUser.role as RoleType,
-				);
-			}
+      if (by === 'roles' && accessorsToScanFor) {
+        can = (accessorsToScanFor as RoleType[]).includes(
+          currentUser.role as RoleType,
+        );
+      }
 
-			if (!accessorsToScanFor) {
-				can = Boolean(currentUser.email);
-			}
+      if (!accessorsToScanFor) {
+        can = Boolean(currentUser.email);
+      }
 
-			if (!can && by === "roles") {
-				return errorResponse(
-					res,
-					"User is not authorized to perform this action",
-					StatusCodes.UNAUTHORIZED,
-					{ [`${by}_required`]: access },
-				);
-			}
+      if (!can && by === 'roles') {
+        return errorResponse(
+          res,
+          'User is not authorized to perform this action',
+          StatusCodes.UNAUTHORIZED,
+          { [`${by}_required`]: access },
+        );
+      }
 
-			if (currentUser && !by && !access) {
-				can = true;
-			}
+      if (currentUser && !by && !access) {
+        can = true;
+      }
 
-			if (!can) {
-				return errorResponse(
-					res,
-					"User is not authenticated",
-					StatusCodes.UNAUTHORIZED,
-					access,
-				);
-			}
+      if (!can) {
+        return errorResponse(
+          res,
+          'User is not authenticated',
+          StatusCodes.UNAUTHORIZED,
+          access,
+        );
+      }
 
-			if (currentUser) {
-				req.user = { ...currentUser, sub: currentUser._id };
-			}
-		} catch (err) {
-			return errorResponse(
-				res,
-				(err as Error).message,
-				StatusCodes.UNAUTHORIZED,
-				access,
-			);
-		}
+      if (currentUser) {
+        req.user = { ...currentUser, sub: currentUser._id };
+      }
+    } catch (err) {
+      return errorResponse(
+        res,
+        (err as Error).message,
+        StatusCodes.UNAUTHORIZED,
+        access,
+      );
+    }
 
-		next?.();
-	};
+    next?.();
+  };
